d8b47e597f5150dab0b6a4a85cbc295537e955b7
peter <elperi187@hotmail.com>
2019-02-01 21:29:46 -0600

    mi primer proyecto android

--- app/src/main/java/unitec/org/elementosandroid/ElementosAndroid.kt
+++ app/src/main/java/unitec/org/elementosandroid/ElementosAndroid.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class ElementosAndroid : AppCompatActivity() {
-
+//////
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elementos_android)
